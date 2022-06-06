552467b34fe25ba0d916f27d5e2d59a43ecc9670
PrachiDesai1311 <68558750+PrachiDesai1311@users.noreply.github.com>
2022-06-06 19:33:15 +0530

    Add files via upload

---
+++ js/quiz.js
@@ -0,0 +1,54 @@
+let quiz=[
+    {
+        question:"How can a datatype be declared to be a constant type?",
+        option:[
+            "1.var",
+            "2.let",
+            "3.constant",
+            "4.const",
+        ],
+        answer:4,
+    },
+    {
+        question:"JavaScript is an ........... language?",
+        option:[
+            "1.Object-Oriented",
+            "2.Object-Based",
+            "3.Procedural",
+            "4.None of the Above",
+            
+        ],
+        answer:1,
+    },
+    {
+        question:"Which of the following keywords is used to define a variable in Javascript?",
+        option:[
+            "1.var",
+            "2.let",
+            "3.Both 1 and 2",
+           "4.None of the above",   
+        ],
+        answer:3,
+    },
+    {
+        question:"Whats so great about XML?",
+        option:[
+            "1.Easy data exchange",
+            "2.High speed on network ",
+            "3.Both",
+            "4.None",   
+        ],
+        answer:3,
+
+    },
+    {
+        question:"In the JavaScript, which one of the following is not considered as an error:",
+        option:[
+            "1.Syntax error",
+            "2.Missing of semicolons",
+            "3.Division by zero",
+            "4.Missing of Bracket",   
+        ],
+        answer:3,
+    }
+]
